8cce78d72dc79df3786ba515743e1ebf84a42cfa
shenttt123 <98625344+shenttt123@users.noreply.github.com>
2022-04-14 22:03:30 -0400

    Create twosum.py

---
+++ 1twosum/twosum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int):
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+
+
+            if remaining in seen:
+                return [i, seen[remaining]]
+
+            seen[value] = i
+        
