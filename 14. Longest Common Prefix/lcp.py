e0bc858e23e7356a51db53c992d78deaa6e7a49b
shenttt123 <98625344+shenttt123@users.noreply.github.com>
2022-04-19 00:39:05 -0400

    Update lcp.py

--- 14. Longest Common Prefix/lcp.py
+++ 14. Longest Common Prefix/lcp.py
@@ -1,11 +1,13 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        shortest = len(min(strs,key=len))     
-        
-        for stringindex in range(1, len(strs)):          
+    def longestCommonPrefix(strs: List[str]):
+        shortest = len(min(strs, key=len))
+
+        for stringindex in range(1, len(strs)):
             for charindex in range(shortest):
-                if strs[0][charindex] != strs[stringindex][charindex]:         
-                    return strs[0][:charindex]
+                if strs[0][charindex] != strs[stringindex][charindex]:
+                    shortest=charindex
+                    break
+        return strs[0][:shortest]
 
         
         
